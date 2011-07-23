8959e77b7f30d0f432c50a8bd77088502eb47f43
progressbarsk <marek@keram.name>
2011-07-23 11:13:11 +0200

    cleaning

--- app/controllers/admin/fees_controller.rb
+++ app/controllers/admin/fees_controller.rb
@@ -2,8 +2,7 @@ module Admin
   class FeesController < Admin::BaseController
 
     crudify :fee
-#            :title_attribute => 'from_account', :xhr_paging => true
-    
+
     def index
       if params[:search]
         @fees = Fee.search(params[:search], params[:page], current_user)
@@ -11,7 +10,7 @@ module Admin
         @fees = Fee.mine(params[:page], current_user)
       end
       
-      render(:layout => false,  :partial => 'fees') if request.xhr?
+      render(:layout => false, :partial => 'fees') if request.xhr?
     end
   end
 end
